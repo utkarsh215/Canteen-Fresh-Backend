d794cfb237bd484df6f414ac8c790864968fb49e
utkarsh215 <115106879+utkarsh215@users.noreply.github.com>
2024-07-26 14:50:21 +0530

    Update index.js
    updated cors

--- index.js
+++ index.js
@@ -46,7 +46,12 @@ app.use(express.json());        //to get data through json
 app.use(express.urlencoded({ extended: "true" }));     //to get data through url
 app.use(express.static("public"));
 app.use(passport.initialize());     //initializing passport
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL, // Allow this specific origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use("/api",instamojo);
 const io = new Server(server,{ cors: {origin: [process.env.CLIENT_URL],} });
 app.use(session({
