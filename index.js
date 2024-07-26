27253d97e29063b7db7a48f4727c64189d069586
utkarsh215 <115106879+utkarsh215@users.noreply.github.com>
2024-07-26 14:15:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -340,23 +340,16 @@ app.post("/myorders", async (req, res) => {
         const data = req.body.data
         const userData = req.body.user
         console.log(req.body.user)
-
-        // data.map(async (item) => {
-        //     const [result] = await db.query("INSERT INTO myorders(item_id,user_id,name,price,quantity,payment,time,date,shop,shop_id,first_name,last_name,enroll_id,image,completed,rejected) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-        //         [item.item_id, userData.user_id, item.name, item.price, item.quantity, item.payment, item.time, item.date, item.shop,item.shop_id,userData.first_name,userData.last_name,userData.enroll_id,item.imageUrl,item.completed,item.rejected]);
-        // })
-
         await Promise.all(data.map(async (item) => {
             await db.query("INSERT INTO myorders(item_id,user_id,name,price,quantity,payment,time,date,shop,shop_id,first_name,last_name,enroll_id,image,completed,rejected) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
                 [item.item_id, userData.user_id, item.name, item.price, item.quantity, item.payment, item.time, item.date, item.shop, item.shop_id, userData.first_name, userData.last_name, userData.enroll_id, item.imageUrl, item.completed, item.rejected]);
         }));
-
         console.log(data);
 
           if(data[0].payment === "counter")
           {
               console.log("In if part")
-            res.redirect(`${process.env.CLIENT_URL}/PaymentSuccess?status=true`);
+           return res.redirect(`${process.env.CLIENT_URL}/PaymentSuccess?status=true`);
           }
           else{
             console.log("in else part");
